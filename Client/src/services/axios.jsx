e920fd67f0f1faf8639d48d207ef7967467268ab
Jinsy <jinsypanamkattu@gmail.com>
2025-03-18 10:12:47 -0700

    updated the live render link

--- Client/src/services/axios.jsx
+++ Client/src/services/axios.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5005/api',
+    baseURL: 'https://partypilot-rb4j.onrender.com/api',
     headers: { 'Content-Type': 'application/json' },
   });
 
